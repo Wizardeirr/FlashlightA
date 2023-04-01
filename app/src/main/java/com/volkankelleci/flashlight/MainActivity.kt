5c6c84f3dbd8b34f9668ecfa568c4a4f3b1cb254
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-04-01 18:52:54 +0300

    CAMERA Permission added

--- app/src/main/java/com/volkankelleci/flashlight/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/flashlight/MainActivity.kt
@@ -2,10 +2,24 @@ package com.volkankelleci.flashlight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.volkankelleci.flashlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //view Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.button.setOnClickListener{
+
+
+        }
     }
 }
